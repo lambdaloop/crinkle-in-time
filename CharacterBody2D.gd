3fe1d94a73c2656900169fc3b59125011014f148
Lili Karashchuk <krchtchk@gmail.com>
2024-01-09 18:45:07 -0800

    initial commit

---
+++ CharacterBody2D.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	#velocity.x = SPEED
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = 0 # move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+
+func _on_mouse_entered():
+	print("mouse entered")
+
+
+func _on_static_body_2d_mouse_entered():
+	print("mouse 2 entered") # Replace with function body.
+
+
+func _on_static_body_2d_mouse_shape_entered(shape_idx):
+	print("mouse entered 3") 
