5da6ca0ec9aee70eb8050c3b1dbce5c8f9125d32
Joshua Vasquez <joshua.vasquez@alleninstitute.org>
2024-01-12 00:32:29 -0800

    catch signals from any connection pt

--- ConnectionPoint.gd
+++ ConnectionPoint.gd
@@ -1,7 +1,11 @@
+class_name ConnectionPoint
 extends Area2D
-signal is_clicked
 
-# Called when the node enters the scene tree for the first time.
+
+
+signal is_clicked(from_who)
+
+# Called when the node enter_inits the scene tree for the first time.
 func _ready():
     input_pickable = true
     self.connect("input_event", _on_Area2D_input_event)
@@ -10,8 +14,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
     pass
+    
+#func get_class():
+#    return "ConnectionPoint"
 
 func _on_Area2D_input_event(viewport: Node, event: InputEvent, shape_idx: int):
     if event.is_pressed() && event.button_index == MOUSE_BUTTON_LEFT:
-       #printf("I'm clicked!W")
-        is_clicked.emit()
+        #print("I'm clicked!")
+        is_clicked.emit(self)
--- level2.gd
+++ level2.gd
@@ -3,10 +3,17 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
-    
 
+    #print(self.get_tree())
+    for child in get_children():
+        if child is ConnectionPoint:
+            child.connect("is_clicked", _on_any_connection_pt_clicked)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
     pass
+    
+func _on_any_connection_pt_clicked(connection_pt):
+    # line drawing logic here.
+    print("(%d, %d)" % [connection_pt.position.x, connection_pt.position.y])
+    #print("yo.")
