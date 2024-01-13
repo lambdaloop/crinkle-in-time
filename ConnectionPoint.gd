c592393b022a313e54d0fd6278f79e6fd7f493c0
Joshua Vasquez <joshua.vasquez@alleninstitute.org>
2024-01-12 23:42:15 -0800

    click pts to make segments

--- ConnectionPoint.gd
+++ ConnectionPoint.gd
@@ -8,17 +8,14 @@ signal is_clicked(from_who)
 # Called when the node enter_inits the scene tree for the first time.
 func _ready():
     input_pickable = true
-    self.connect("input_event", _on_Area2D_input_event)
+    self.connect("input_event", _on_collision_obj2d_input_event)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
     pass
-    
-#func get_class():
-#    return "ConnectionPoint"
 
-func _on_Area2D_input_event(viewport: Node, event: InputEvent, shape_idx: int):
+func _on_collision_obj2d_input_event(viewport: Node, event: InputEvent, shape_idx: int):
     if event.is_pressed() && event.button_index == MOUSE_BUTTON_LEFT:
         #print("I'm clicked!")
         is_clicked.emit(self)
