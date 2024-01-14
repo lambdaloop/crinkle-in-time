0cfc6f7ad0d774f5443a43bca1ca156dd65eeae8
Joshua Vasquez <joshua.vasquez@alleninstitute.org>
2024-01-14 01:07:08 -0800

    add game over screen

--- level1.gd
+++ level1.gd
@@ -3,11 +3,18 @@ extends Node2D
 @onready var background = $Background
 @onready var removable = $removable
 
+var game_over_scene = preload("res://game_over.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
     pass
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
     pass
+
+func handle_death():
+    var game_over_screen = game_over_scene.instantiate()
+    self.add_child(game_over_screen)
+    get_tree().paused = true
+    
